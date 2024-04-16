06f7fdeaf8855da88d47544bfc5981ac47b03f86
damianfly1 <damianmucha105@gmail.com>
2024-04-16 17:02:00 +0200

    init

---
+++ baza_v1/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Lotnisko
+class Airport(models.Model):
+    site_num = models.CharField(max_length=255, unique=True)
+    fac_type = models.CharField(max_length=255)
+    loc_id = models.CharField(max_length=255)
+    eff_date = models.DateField()
+
+# Właściciel
+class Owner(models.Model):
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    owner_name = models.CharField(max_length=255)
+    owner_addr = models.TextField()
+    owner_city = models.CharField(max_length=255)
+    owner_phon = models.CharField(max_length=255)
+
+# Zarządca
+class Manager(models.Model):
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    manager_name = models.CharField(max_length=255)
+    manager_addr = models.TextField()
+    manager_city = models.CharField(max_length=255)
+    manager_phon = models.CharField(max_length=255)
+
+# Punkty odniesienia
+class ReferencePoint(models.Model):
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    point_name = models.CharField(max_length=255)
+
+# Statki powietrzne
+class BasedAircraft(models.Model):
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    aircraft_type = models.CharField(max_length=255)
