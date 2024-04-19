423aa6c6c8d385c48abcfc973005ed52930322af
damianfly1 <damianmucha105@gmail.com>
2024-04-19 21:40:49 +0200

    dalsze

--- baza_v1/models.py
+++ baza_v1/models.py
@@ -2,51 +2,57 @@ from django.db import models
 
 # Lotnisko
 class Airport(models.Model):
-    site_num = models.CharField(max_length=255, unique=True)
-    fac_type = models.CharField(max_length=255)
-    loc_id = models.CharField(max_length=255)
-    eff_date = models.DateField()
-    
+    site_number = models.CharField(max_length=255, unique=True) #kod lotniska
+    type = models.CharField(max_length=255) #typ lotniska
+    location_id = models.CharField(max_length=255) #id lokalizacji
+    date = models.DateField() #data dodania danych, mapuje się do eff_date
+    region_code = models.CharField(max_length=255) #kod regionu
+    jet_count = models.IntegerField() #liczba myśliwców
+    helicopter_count = models.IntegerField() #liczba helikopterów
+    military_aircraft_count = models.IntegerField() #liczba militarnych
+    commercial_aircraft_count = models.IntegerField() #liczba cywlinych 
+    flight_count = models.IntegerField() #ilość lotów (Ga_Local_O)
+
     class Meta:
         db_table = "Airport"
 
 # Właściciel
 class Owner(models.Model):
-    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
-    owner_name = models.CharField(max_length=255)
-    owner_addr = models.TextField()
-    owner_city = models.CharField(max_length=255)
-    owner_phon = models.CharField(max_length=255)
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE) #lotnisko
+    owner_name = models.CharField(max_length=255) #imie właściciela
+    owner_addr = models.TextField() #adres właściciela
+    owner_city = models.CharField(max_length=255) #miasto właściciela
+    owner_phon = models.CharField(max_length=255) #telefon właściciela
 
     class Meta:
         db_table = "Owner"
 
 # Zarządca
 class Manager(models.Model):
-    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
-    manager_name = models.CharField(max_length=255)
-    manager_addr = models.TextField()
-    manager_city = models.CharField(max_length=255)
-    manager_phon = models.CharField(max_length=255)
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE) #lotnisko
+    manager_name = models.CharField(max_length=255) #imie menedzera
+    manager_addr = models.TextField() #adres menedzera
+    manager_city = models.CharField(max_length=255) #miasto menedzera
+    manager_phon = models.CharField(max_length=255) #telefon menedzera
 
     class Meta:
         db_table = "Manager"
 
 # Punkty odniesienia
 class ReferencePoint(models.Model):
-    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
-    ref_point_1 = models.CharField(max_length=255, default='')
-    ref_point_2 = models.CharField(max_length=255, default='')
-    ref_point_3 = models.CharField(max_length=255, default='')
-    ref_point_4 = models.CharField(max_length=255, default='')
+    airport = models.ForeignKey(Airport, on_delete=models.CASCADE) #lotnisko
+    ref_point_1 = models.CharField(max_length=255, default='') #punkt1
+    ref_point_2 = models.CharField(max_length=255, default='') #punkt2
+    ref_point_3 = models.CharField(max_length=255, default='') #punkt2
+    ref_point_4 = models.CharField(max_length=255, default='') #punkt3
 
     class Meta:
         db_table = "ReferencePoint"
 
-# Statki powietrzne
-class BasedAircraft(models.Model):
-    airport = models.ForeignKey(Airport, on_delete=models.CASCADE)
-    aircraft_type = models.CharField(max_length=255)
+# Przypisany statek powietrzny
+# class BasedAircraft(models.Model):
+#     airport = models.ForeignKey(Airport, on_delete=models.CASCADE) #lotnisko
+#     aircraft_type = models.CharField(max_length=255) #typ statku powietrznego
 
-    class Meta:
-        db_table = "BasedAircraft"
+#     class Meta:
+#         db_table = "BasedAircraft"
